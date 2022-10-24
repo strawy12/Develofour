95b84e41f2bf971df41fee1e0c129963c0ad9707
strawy12 <strawy6842@naver.com>
2022-10-24 20:37:33 +0900

    Window 제작 시작

---
+++ Assets/02.Scripts/UI/Window/BaseWindow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Window : MonoBehaviour
+{
+    private string windowName;
+
+    private Button closeBtn; 
+    /// <summary>
+    /// 최소화 버튼
+    /// </summary>
+    private Button minimizationBtn;
+    /// <summary>
+    /// 전체화면 버튼
+    /// </summary>
+    private Button maximizeBtn;
+
+
+}
