8610651784cd0f44eba0125b1e80318dbfe01269
injun2005 <77712212+injun2005@users.noreply.github.com>
2022-11-02 00:57:35 +0900

    windowSO

---
+++ Assets/02.Scripts/UI/Window/WindowBar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowBar : MonoBehaviour
+{
+    [SerializeField] private Button maximumBtn;
+    [SerializeField] private Button minimumBtn;
+    [SerializeField] private Button closeBtn;
+
+
+}
