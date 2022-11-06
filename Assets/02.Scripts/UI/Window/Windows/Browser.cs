3f8474c87c66d803918cf6a29b7cee71caeb4cd8
435yj <81200320+435yj@users.noreply.github.com>
2022-11-07 00:53:27 +0900

    browser 거의 다 하하하함?

--- Assets/02.Scripts/UI/Window/Windows/Browser.cs
+++ Assets/02.Scripts/UI/Window/Windows/Browser.cs
@@ -1,18 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Browser : MonoBehaviour
+
+// 임시 EsiteLing
+public enum ESiteLink
+{ 
+    None,
+    Chrome,
+    Youtube,
+}
+
+
+
+public class Browser : Window
 {
-    // Start is called before the first frame update
+    private Dictionary<ESiteLink, Site> siteDictionary;
+    
+    private Site usingSite;
+    
+    private Stack<Site> undoSite;
+    private Stack<Site> redoSite;
+
+    private Transform siteParent;
+
+    private BrowserBar browserBar;
+
     void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        undoSite = new Stack<Site>();
+        redoSite = new Stack<Site>();
+    }
+
+    private void BindingStart()
+    {
+        siteDictionary.Add(ESiteLink.Chrome, usingSite);
+        undoSite.Push(usingSite);
+    }
+
+    public void ChangeSite(ESiteLink eSiteLink)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeSite(string siteString)
     {
-        
+
+    }
+
+    public void UndoSite()
+    {
+        redoSite.Push(undoSite.Pop()); // 맨 위에거는 redo로 
+        usingSite = undoSite.Pop(); // 그 다음거는 현재 상태로 back
+    }
+
+    public void RedoSite()
+    {
+        undoSite.Push(redoSite.Pop());
+        usingSite = redoSite.Pop(); 
+        // 작동은 UndoSite함수의 정 반대로 
+    }
+
+    public void ResetBrowser()
+    {
+        usingSite = null;
+
+        undoSite.Clear();
+        redoSite.Clear();
     }
 }
