303a706a655be01391f75efdf8ae9a5f2788e072
snowgaru <77821586+snowgaru@users.noreply.github.com>
2022-11-07 22:27:09 +0900

    site 완성

--- Assets/02.Scripts/Core/ESiteLink.cs
+++ Assets/02.Scripts/Core/ESiteLink.cs
@@ -1,18 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ESiteLink : MonoBehaviour
+public enum ESiteLink 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    None = -1,
 }
