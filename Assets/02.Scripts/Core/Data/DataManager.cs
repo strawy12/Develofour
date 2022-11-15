8f16387d01c9440d68999f6fad12473bc5e90563
OIF <strawy6842@naver.com>
2022-11-16 01:07:04 +0900

    Data 오류 수정

--- Assets/02.Scripts/Core/Data/DataManager.cs
+++ Assets/02.Scripts/Core/Data/DataManager.cs
@@ -15,39 +15,25 @@ public class DataManager : MonoSingleton<DataManager>
 
     private void Awake()
     {
-<<<<<<< HEAD
-        //Application.persistentDataPath : ����ڵ��丮 / AppData / LocalLow / ȸ���̸� / ���δ�Ʈ�̸�
+        //Application.persistentDataPath : ����ڵ��丮 / AppData / LocalLow / ȸ���̸� / ���δ�Ʈ�̸�
         SAVE_PATH = Application.persistentDataPath + "/saves/";
     }
 
     private void CheckDirectory()
     {
-        if(!Directory.Exists(SAVE_PATH))
+        if (!Directory.Exists(SAVE_PATH))
         {
             Directory.CreateDirectory(SAVE_PATH);
         }
-    }
-=======
         CreatePlayerData();
-        // SAVE_PATH = Application.dataPath
 
     }
-
-    //private void CheckDirectory()
-    //{
-    //    if(/*해당 Path에 Directory가 없다면*/)
-    //    {
-    //        // Directory 생성
-    //    }
-    //}
->>>>>>> LoginCheck
 
     private void CreatePlayerData()
     {
         playerData = new PlayerData();
     }
 
-<<<<<<< HEAD
     private void LoadFromJson()
     {
         if (File.Exists(SAVE_PATH + SAVE_FILE))
@@ -59,23 +45,7 @@ public class DataManager : MonoSingleton<DataManager>
         {
             CreatePlayerData();
         }
-=======
-    //private void LoadFromJson()
-    //{
-    //    if (/*해당 PATH에 해당 파일이 존재한다면*/)
-    //    {
-    //        // 해당 Json를 불러와서 PlayerData 클래스 형식으로 변환 시키기
-    //    }
-    //    else
-    //    {
-    //        CreatePlayerData();
-    //    }
->>>>>>> LoginCheck
-
-    //    SaveToJson();
-    //}
-
-<<<<<<< HEAD
+    }
     private void SaveToJson()
     {
         CheckDirectory();
@@ -83,20 +53,14 @@ public class DataManager : MonoSingleton<DataManager>
         string data = JsonUtility.ToJson(playerData);
         File.WriteAllText(SAVE_PATH, data);
     }
-=======
-    //private void SaveToJson()
-    //{
-    //    // playerData를 json 형식의 string으로 바꿔주고 파일에다가 이 스트링 값을 써준다
-    //}
->>>>>>> LoginCheck
 
-    //private void OnDestroy()
-    //{
-    //    SaveToJson();
-    //}
+    private void OnDestroy()
+    {
+        SaveToJson();
+    }
 
-    //private void OnApplicationQuit()
-    //{
-    //    SaveToJson();
-    //}
+    private void OnApplicationQuit()
+    {
+        SaveToJson();
+    }
 }
