8efb686a53f3017b692be2fc2d44e3deca3432ad
injun2005 <77712212+injun2005@users.noreply.github.com>
2022-11-15 22:27:28 +0900

    QuestNotice

--- Assets/02.Scripts/UI/Notice/NoticeSystem.cs
+++ Assets/02.Scripts/UI/Notice/NoticeSystem.cs
@@ -54,18 +54,7 @@ public class NoticeSystem : MonoUI
 
         EventManager.StartListening(EEvent.ClickNoticeBtn, ToggleNotice);
         EventManager.StartListening(EEvent.LeftButtonClick, CheckClose);
-
-        EventManager.StartListening(EEvent.HateBtnClicked, HateBtnClicked);
     }
-
-    private void HateBtnClicked(object emptyParam)
-    {
-        NoticeData data = new NoticeData();
-        data.head = "경찰 출두장 메일 확인하기";
-        data.body = "즐겨찾기에 있는 메일 바로가기 버튼을 이용해 갈 수 있습니다";
-        ShowNoticePanel(data);
-    }
-
     private void CheckClose(object hits)
     {
         if (isOpen == false) return;
---
+++ Assets/02.Scripts/UI/Notice/QuestNotice.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestNotice : MonoBehaviour
+{
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    { 
+        EventManager.StartListening(EEvent.HateBtnClicked, HateBtnClicked);
+    }
+
+    private void HateBtnClicked(object emptyParam)
+    {
+        NoticeData data = new NoticeData();
+        data.head = "경찰 출두장 메일 확인하기";
+        data.body = "즐겨찾기에 있는 메일 바로가기 버튼을 이용해 갈 수 있습니다";
+        NoticeSystem.OnGeneratedNotice(data);
+
+        EventManager.StopListening(EEvent.HateBtnClicked,HateBtnClicked);
+    }
+}
