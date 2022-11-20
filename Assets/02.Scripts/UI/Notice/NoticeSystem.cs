978d77725eb02107381633089a276dd2273703c6
435yj <yujin050420@naver.com>
2022-11-20 21:42:44 +0900

    수정

--- Assets/02.Scripts/UI/Notice/NoticeSystem.cs
+++ Assets/02.Scripts/UI/Notice/NoticeSystem.cs
@@ -54,18 +54,8 @@ public class NoticeSystem : MonoUI
 
         EventManager.StartListening(EEvent.ClickNoticeBtn, ToggleNotice);
         EventManager.StartListening(EEvent.LeftButtonClick, CheckClose);
-
-        EventManager.StartListening(EEvent.EndTutorialScene, EndTutorialScene);
     }
-
-    private void EndTutorialScene(object emptyObj)
-    {
-        NoticeData data = new NoticeData();
-        data.head = "AI 규제 법에 반대하기";
-        data.body = "영상의 싫어요 버튼을 누름으로써 AI 규제 법에 대한 생각을 나타내세요";
-        ShowNoticePanel(data);
-    }
-
+ 
     private void CheckClose(object hits)
     {
         if (isOpen == false) return;
