31a979b3e4c05804254b8ae614fa283f2e875649
435yj <yujin050420@naver.com>
2022-11-21 23:52:46 +0900

    좋아요 싫어요 상세 개발

--- Assets/02.Scripts/UI/Brwoser/Site/Sites/Youtube/YoutubeInteractionButton.cs
+++ Assets/02.Scripts/UI/Brwoser/Site/Sites/Youtube/YoutubeInteractionButton.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -10,11 +11,15 @@ public class YoutubeInteractionButton : MonoBehaviour
     [SerializeField] private bool isHateBtn;
 
     private bool isClicked = false;
+    private bool isOverLapLike = false;
+    private bool isOverLapHate = false;
 
     private float durationTime = 0.4f;
 
     [SerializeField]
     private Image otherImage;
+    [SerializeField]
+    private TMP_Text hatePeopleText;
 
     private Button button;
     private Image image;
@@ -40,28 +45,60 @@ public class YoutubeInteractionButton : MonoBehaviour
     {
         if(!isClicked)
         {
-
             isClicked = true;
             Sequence seq = DOTween.Sequence();
 
             //before
-            seq.Append(rectTransform.DOScale(originScale * 1.3f, durationTime));
+            seq.Append(rectTransform.DOScale(originScale * 1.2f, durationTime));
 
             if (isHateBtn)
             {
-                image.enabled = true;
-                otherImage.enabled = false;
+                if(!isOverLapHate)
+                {
+                    isOverLapHate = true;
+                    image.enabled = true;
+                    otherImage.enabled = false;
 
-                seq.Join(image.DOColor(Color.yellow, durationTime));
-                seq.Join(otherImage.DOColor(Color.white, durationTime));
+                    hatePeopleText.text = "73";
+
+                    seq.Join(image.DOColor(Color.yellow, durationTime));
+                    seq.Join(otherImage.DOColor(Color.white, durationTime));
+                }
+                else if(isOverLapHate)
+                {
+                    isOverLapHate = false;
+                    image.enabled = false;
+
+                    hatePeopleText.text = "72";
+
+                    seq.Join(image.DOColor(Color.white, durationTime));
+                    seq.Append(rectTransform.DOScale(originScale, durationTime));
+                    return;
+                }
+
             }
             if (!isHateBtn)
             {
-                image.enabled = true;
-                otherImage.enabled = false;
+                if(!isOverLapLike)
+                {
+                    isOverLapLike = true;
+                    image.enabled = true;
+                    otherImage.enabled = false;
 
-                seq.Join(image.DOColor(Color.red, durationTime));
-                seq.Join(otherImage.DOColor(Color.white, durationTime));
+                    hatePeopleText.text = "72";
+
+                    seq.Join(image.DOColor(Color.red, durationTime));
+                    seq.Join(otherImage.DOColor(Color.white, durationTime));
+                }
+                else if (isOverLapLike)
+                {
+                    isOverLapLike = false;
+                    image.enabled = false;
+
+                    seq.Join(image.DOColor(Color.white, durationTime));
+                    seq.Append(rectTransform.DOScale(originScale, durationTime));
+                    return;
+                }
             }
 
             seq.Join(rectTransform.DOAnchorPosX(rectTransform.anchoredPosition.x - 1f, durationTime));
