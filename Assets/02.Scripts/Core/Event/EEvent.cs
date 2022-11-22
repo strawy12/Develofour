2f88335a8e0d4e1b03a57520d5a0a2834a113ed1
OIF <strawy6842@naver.com>
2022-11-22 22:48:29 +0900

    EEvent 분류 완료

--- Assets/02.Scripts/Core/Event/EEvent.cs
+++ Assets/02.Scripts/Core/Event/EEvent.cs
@@ -1,27 +1,65 @@
-public enum EEvent 
+public enum ECoreEvent 
 {
     None = -1,
-    LeftButtonClick = 1,
-    GeneratedNotice,
-    OpenNoticeSystem,
-    ClickNoticeBtn,
+    LeftButtonClick,
     ChangeBGM,
-    ActivePowerPanel,
-    ExpendMenu,
     OpenTextBox,
-    ShowCutScene,
-    AddFavoriteSite,
-    ResetSite,
-    ClickHateBtn,
-    LoginGoogle,
-    SkipCutScene,
-    EndNewsCutScene,
-    End
 }
-
 public enum EQuestEvent
 {
     None = -1,
-    HateBtnClicked = 10001,
+    HateBtnClicked,
     LoginGoogle,
 }
+
+public enum EWindowEvent
+{
+    None = -1,
+    ClickNoticeBtn,
+    ActivePowerPanel,
+}
+
+public enum ENoticeEvent
+{
+    None = -1,
+    GeneratedNotice,
+    OpenNoticeSystem,
+    ClickNoticeBtn,
+}
+
+public enum EBrowserEvent
+{
+    None = -1,
+    AddFavoriteSite,
+}
+
+#region Site
+public enum ESiteEvent
+{
+    None = -1,
+    ResetSite,
+}
+
+public enum EYoutubeSiteEvent
+{
+    None = -1,
+    ClickHateBtn,
+}
+
+public enum ELoginSiteEvent
+{
+    None = -1,
+    LoginSuccess,
+}
+
+#endregion
+
+public enum ECutSceneEvent
+{
+    None = -1,
+    ShowCutScene ,
+    SkipCutScene,
+    EndNewsCutScene,
+}
+
+
