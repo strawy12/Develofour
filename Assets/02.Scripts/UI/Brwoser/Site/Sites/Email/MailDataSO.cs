fbe26dec8ac3f34276d1904163852a5939e07b0c
snowgaru <77821586+snowgaru@users.noreply.github.com>
2022-11-23 00:32:37 +0900

    오류 수정

--- Assets/02.Scripts/UI/Brwoser/Site/Sites/Email/MailDataSO.cs
+++ Assets/02.Scripts/UI/Brwoser/Site/Sites/Email/MailDataSO.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "SO/Window/Mail/Data")]
 public class MailDataSO : ScriptableObject
 {
-    public EEmailCategory emailType;
+    public Mail mailType;
     public string nameText;
     public string informationText;
     public string timeText;
     public bool isHighlighted;
+    public EmailLine mailObject;
 }
