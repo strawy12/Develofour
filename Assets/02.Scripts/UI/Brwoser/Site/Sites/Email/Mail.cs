06658b1fecbf01e882b2f9a2dec279669439d6e0
snowgaru <77821586+snowgaru@users.noreply.github.com>
2022-11-23 18:12:46 +0900

    해줘인준

--- Assets/02.Scripts/UI/Brwoser/Site/Sites/Email/Mail.cs
+++ Assets/02.Scripts/UI/Brwoser/Site/Sites/Email/Mail.cs
@@ -2,13 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Mail : MonoBehaviour
 {
+    #region ¹öÆ°µé
+    [SerializeField]
+    private Button mailCloseButton;
 
+    [SerializeField]
+    private Button mailDestroyButton;
+
+    [SerializeField]
+    private Button mailHighlightedButton;
+    #endregion
     [SerializeField]
     protected EEmailCategory mailType;
 
+    [SerializeField]
     protected MailDataSO mailData;
 
     public MailDataSO MailData { get { return mailData; } }
@@ -16,6 +27,9 @@ public abstract class Mail : MonoBehaviour
     public virtual void Init(MailDataSO mailData)
     {
         this.mailData = mailData;
+        mailCloseButton.onClick.AddListener(HideMail);
+        mailDestroyButton.onClick.AddListener(DestroyMail);
+        mailHighlightedButton.onClick.AddListener(HighlightedMail);
     }
 
     public virtual void ShowMail()
@@ -26,5 +40,18 @@ public abstract class Mail : MonoBehaviour
     public virtual void HideMail()
     {
         gameObject.SetActive(false);
+    }
+
+    public virtual void DestroyMail()
+    {
+        mailType = EEmailCategory.Remove;
+        mailData.emailType = EEmailCategory.Remove;
+        HideMail();
+    }
+
+    public virtual void HighlightedMail()
+    {
+        mailType = EEmailCategory.Highlighted;
+        mailData.emailType = EEmailCategory.Highlighted;
     }
 }
