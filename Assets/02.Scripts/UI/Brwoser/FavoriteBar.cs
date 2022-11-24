b0b03e9a169b85b5f05915530febb3df19169317
OIF <strawy6842@naver.com>
2022-11-25 01:24:35 +0900

    Youtube 변경 완료

--- Assets/02.Scripts/UI/Brwoser/FavoriteBar.cs
+++ Assets/02.Scripts/UI/Brwoser/FavoriteBar.cs
@@ -29,6 +29,7 @@ public class FavoriteBar : MonoBehaviour
     
     private void MakeFavoriteButton(Site site)
     {
+        return;
         ESiteLink siteLink = site.SiteLink;
         BrowserFavoriteButton button = Instantiate(favoriteBtnPrefab, favoritesParent);
         button.SiteLink = siteLink;
