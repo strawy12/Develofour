84be4298e8be86283cb29167f01e71c155d3ab06
435yj <yujin050420@naver.com>
2022-11-24 20:16:22 +0900

    메인병합

--- Assets/02.Scripts/Utils/Define.cs
+++ Assets/02.Scripts/Utils/Define.cs
@@ -77,16 +77,4 @@ public static class Define
     {
         return new Color(Random.value, Random.value, Random.value, Random.value);
     }
-
-    public static Vector2 CanvasMousePos
-    {
-        get
-        {
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = 0f;
-            mousePos -= (Vector3)Constant.MAXCANVASPOS;
-
-            return mousePos;
-        }
-    }
 }
