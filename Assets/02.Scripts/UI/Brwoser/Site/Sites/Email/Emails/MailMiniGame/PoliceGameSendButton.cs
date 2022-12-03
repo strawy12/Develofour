1e23a5aa603dbd18f9018d00027146a8d00fc722
OIF <strawy6842@naver.com>
2022-12-04 00:22:23 +0900

    타이핑 기능 추가

--- Assets/02.Scripts/UI/Brwoser/Site/Sites/Email/Emails/MailMiniGame/PoliceGameSendButton.cs
+++ Assets/02.Scripts/UI/Brwoser/Site/Sites/Email/Emails/MailMiniGame/PoliceGameSendButton.cs
@@ -6,14 +6,27 @@ using DG.Tweening;
 public class PoliceGameSendButton : MonoBehaviour
 {
     private RectTransform rectTransform;
+
+    [SerializeField]
+    private float scale = 1.2f;
+    [SerializeField]
+    private Vector2 targetPos = new Vector2(415.5f, 20f);
+    [SerializeField]
+    private float rotateDuration = 0.8f;
+    [SerializeField]
+    private float scaleDuration = 0.3f;
+    [SerializeField]
+    private float posDuration = 0.2f;
     public void SuccessEffect()
     {
         rectTransform ??= GetComponent<RectTransform>();
 
         Sequence seq = DOTween.Sequence();
-        seq.Append(rectTransform.DOAnchorPosY(20, 0.2f));
-        seq.Join(rectTransform.DOLocalRotate(new Vector3(0, 360, 0), 0.8f));
-        seq.Join(rectTransform.DOScale(1.2f, 0.5f)).OnComplete(() => rectTransform.DOScale(1f, 0.3f));
-        seq.Insert(0.6f, rectTransform.DOAnchorPosX(0, 0.2f));
+        seq.Append(rectTransform.DOAnchorPosY(targetPos.y, posDuration));
+        seq.Join(rectTransform.DORotate(new Vector3(0, 360, 0), rotateDuration, RotateMode.FastBeyond360));
+        seq.Join(rectTransform.DOScale(scale, scaleDuration));
+        seq.Append(rectTransform.DOScale(1f, scaleDuration));
+
+        seq.Insert(0.6f, rectTransform.DOAnchorPosX(targetPos.x, posDuration));
     }
 }
