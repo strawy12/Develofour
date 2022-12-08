025efb782d0891b324c0643cd909e44e48cc5f14
injun2005 <77712212+injun2005@users.noreply.github.com>
2022-12-08 18:02:36 +0900

    facebookFriendPrefab

--- Assets/02.Scripts/UI/Brwoser/Site/Sites/Brunch/PostPanelParent.cs
+++ Assets/02.Scripts/UI/Brwoser/Site/Sites/Brunch/PostPanelParent.cs
@@ -8,12 +8,13 @@ public class PostPanelParent : MonoBehaviour
     private RectTransform blogPostPanelRect;
     [SerializeField]
     private int offsetY;
-
+    [SerializeField]
+    private float spacing = 0;
     private RectTransform rectTransform;
 
-    public void Init(int count)
+    public void ChangeVerticalUICount(int count)
     {
         rectTransform ??= GetComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, offsetY + blogPostPanelRect.sizeDelta.y * count);
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, offsetY + spacing * count + blogPostPanelRect.sizeDelta.y * count);
     }
 }
