ba5a0c008ea1ef6278ec4eb3f1982416b86c5163
snowgaru <lootmusic999@gmail.com>
2022-12-10 17:02:12 +0900

    friendSO 제작

--- Assets/02.Scripts/UI/Brwoser/Site/Sites/Facebook/FacebookFriendPanel.cs
+++ Assets/02.Scripts/UI/Brwoser/Site/Sites/Facebook/FacebookFriendPanel.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class FacebookFriendPanel : MonoBehaviour
 {
-
+    private Image profileImage;
+    private TextMeshProUGUI nameText;
+    private TextMeshProUGUI infoText;
 }
