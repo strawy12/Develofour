9863bac49add2fac776200ed4fabe231e4090dc6
OIF <strawy6842@naver.com>
2023-01-09 20:54:58 +0900

    Discord 관련 스크립트 만들어는 둠

---
+++ Assets/02.Scripts/UI/Window/Windows/Discord/Discord.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Discord : Window
+{
+    List<DiscordChatDataListSO> chatDataList;
+    List<DiscordChatDataListSO> talkDataList;
+
+    // Pooling Ω√Ω∫≈∆ ¡¶¿€
+
+    DiscordChatDataListSO GetChatDataList(string userName)
+    {
+        return null;
+    }
+
+    DiscordMessagePanel CreatePanel(DiscordChatData data)
+    {
+        return null;
+    }
+
+    void SettingChattingPanel(string username)
+    {
+
+    }
+
+}
