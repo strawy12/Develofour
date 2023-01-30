9a2d2559274d7db310ae02cabef25cb04c52cc19
injun2005 <77712212+injun2005@users.noreply.github.com>
2023-01-30 10:45:46 +0900

    pivot

--- Assets/02.Scripts/Utils/Constant.cs
+++ Assets/02.Scripts/Utils/Constant.cs
@@ -9,7 +9,7 @@ public static class Constant
     public static readonly Vector2 WINDOWICONSIZE = new Vector2(100, 100);
     public static readonly Vector2 WINDOWDEFAULTPOS = new Vector2(-890, 460);
 
-    public static readonly Vector2 NOTICE_POS = new Vector2(-17.5f, -400f);
+    public static readonly Vector2 NOTICE_POS = new Vector2(-17.5f, -470f);
 
     public static readonly float NOTICEDRAG_INVISIBILITY = 65.5f;
     public static readonly float NOTICEDRAG_INTERVAL = -45f;
