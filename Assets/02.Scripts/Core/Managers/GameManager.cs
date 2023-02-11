a3ca0137de798b44f06cbdf80b97dc42440fd551
injun2005 <77712212+injun2005@users.noreply.github.com>
2023-02-11 10:19:59 +0900

    스크립트 삭제

--- Assets/02.Scripts/Core/Managers/GameManager.cs
+++ Assets/02.Scripts/Core/Managers/GameManager.cs
@@ -7,19 +7,12 @@ public class GameManager : MonoSingleton<GameManager>
     [SerializeField]
     private ClickEffect clickEffect;
 
-    public bool useCutScene;
-
     private EGameState gameState;
 
     public EGameState GameState { get { return gameState; } }
 
     private void Start()
     {
-        if(useCutScene)
-        {
-            object[] ps = new object[1] { typeof(NewsCutScene) };
-            EventManager.TriggerEvent(ECutSceneEvent.ShowCutScene, ps);
-        }
 
     }
     public void ChangeGameState(EGameState state)
