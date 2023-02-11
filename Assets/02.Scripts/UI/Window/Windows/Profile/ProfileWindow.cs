b8cc9998b3f9fbe4adcccf714253cfaab1f98b3f
injun2005 <77712212+injun2005@users.noreply.github.com>
2023-02-11 21:06:34 +0900

    profile new panels

--- Assets/02.Scripts/UI/Window/Windows/Profile/ProfileWindow.cs
+++ Assets/02.Scripts/UI/Window/Windows/Profile/ProfileWindow.cs
@@ -13,17 +13,42 @@ public class ProfileWindow : Window
     [SerializeField]
     private ProfileChatting profileChatting;
     [SerializeField]
-    private Button infoSearchBtn;
-
+    private InfoCheckPanel infoCheckPanel;
+    [SerializeField]
+    private FileSearchPanel fileSearchPanel;
+    [SerializeField]
+    private Button infoCheckBtn;
+    [SerializeField]
+    private Button fileSearchBtn;
     protected override void Init()
     {
         base.Init();
         profileChatting.Init();
         profilePanel.Init();
         profileSystemBtn.onClick.AddListener(ShowProfileCategoryPanel);
+        infoCheckBtn.onClick.AddListener(ShowInfoCheckPanel);
+        fileSearchBtn.onClick.AddListener(ShowFileSearchPanel);
+    }
+    private void HideAllPanel()
+    {
+        profilePanel.gameObject.SetActive(false);
+        infoCheckPanel.gameObject.SetActive(false);
+        fileSearchPanel.gameObject.SetActive(false);
+
     }
     private void ShowProfileCategoryPanel()
     {
+        HideAllPanel();
         profilePanel.gameObject.SetActive(true);
+    }
+    private void ShowInfoCheckPanel()
+    {
+        HideAllPanel();
+        infoCheckPanel.gameObject.SetActive(true);
+    }
+    private void ShowFileSearchPanel()
+    {
+        HideAllPanel();
+        fileSearchPanel.gameObject.SetActive(true);
     }
 }
