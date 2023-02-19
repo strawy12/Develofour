5971f7ba9a4fe651a131cf3350c4429b51a08f87
snowgaru <lootmusic999@gmail.com>
2023-02-19 22:28:35 +0900

    흔적 지우기 및 windowalterationSO 이식 완료

--- Assets/02.Scripts/SO/WindowAlterationSO.cs
+++ Assets/02.Scripts/SO/WindowAlterationSO.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "SO/Window/Alteration")]
-public class WindowAlterationSO : ScriptableObject 
+public class WindowAlterationSO : SOParent
 {
     [HideInInspector]
     public bool isMaximum;
     public Vector2 size;
     public Vector2 pos;
+
+    public override void Setting(string[] ps)
+    {
+        size.x = int.Parse(ps[1]);
+        size.y = int.Parse(ps[2]);
+        pos.x = int.Parse(ps[3]);
+        pos.y = int.Parse(ps[4]);
+    }
 }
