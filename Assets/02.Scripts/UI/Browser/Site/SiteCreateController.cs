51497df2ec2a78d5917eab3a0ce1a9e8c963fa3d
435yj <yujin050420@naver.com>
2023-02-23 19:04:06 +0900

    no message

---
+++ Assets/02.Scripts/UI/Browser/Site/SiteCreateController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SiteCreateController : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> sitelist = new List<GameObject>(); 
+}
