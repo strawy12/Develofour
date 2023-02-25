d4e94f05acc746f821dbdcf9aead7c43cdb3bfc3
snowgaru <lootmusic999@gmail.com>
2023-02-25 21:15:56 +0900

    정보 텍스트 가져다 대면 밑에 정보뜸

--- Assets/02.Scripts/UI/Window/Windows/Profile/ProfileInfoText.cs
+++ Assets/02.Scripts/UI/Window/Windows/Profile/ProfileInfoText.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ProfileInfoText : MonoBehaviour
+public class ProfileInfoText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public string infoNameKey;
 
     public string afterText;
 
+    public string getInfoText;
+
     public TMP_Text infoText;
+
+    public ProfileShowInfoTextPanel showPanel;
+
+    public float x;
+    public float y;
 
     //이전 텍스트로 변경, 이후 텍스트로 변경해주는 함수
 
@@ -17,5 +25,33 @@ public class ProfileInfoText : MonoBehaviour
     public void ChangeText()
     {
         infoText.text = afterText;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (infoText.text != afterText)
+        {
+            return;
+        }
+        showPanel.text.text = getInfoText;
+        if(eventData.position.y > 200)
+        {
+            Debug.Log(eventData.position.y);
+            Debug.Log(transform.localPosition);
+            showPanel.transform.position = transform.position + new Vector3(0.5f, -0.35f,0);
+        }
+        else
+        {
+            showPanel.transform.position = transform.position + new Vector3(0.5f, 0.35f, 0);
+            //showPanel.transform.position = this.gameObject.transform.position + new Vector3(-0.72f, showPanel.transform.position.y, 0);
+            Debug.Log(eventData.position.y);
+        }
+        showPanel.gameObject.SetActive(true);
+        showPanel.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        showPanel.gameObject.SetActive(false);
     }
 }
---
+++ Assets/02.Scripts/UI/Window/Windows/Profile/ProfileShowInfoTextPanel.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ProfileShowInfoTextPanel : MonoBehaviour
+{
+    public TMP_Text text;
+}
