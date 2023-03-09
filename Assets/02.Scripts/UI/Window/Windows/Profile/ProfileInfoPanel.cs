bfcca1fbd57d00f47d93d43e2b144baa584cd1ad
injun2005 <77712212+injun2005@users.noreply.github.com>
2023-03-09 23:25:18 +0900

    수정중

--- Assets/02.Scripts/UI/Window/Windows/Profile/ProfileInfoPanel.cs
+++ Assets/02.Scripts/UI/Window/Windows/Profile/ProfileInfoPanel.cs
@@ -16,47 +16,32 @@ public class ProfileInfoPart
 }
 
 public class ProfileInfoPanel : MonoBehaviour
-{ 
+{
     public EProfileCategory category;
 
     //동적 저장을 위해서는 활성화 비활성화 여부를 들고있는 SO 혹은 Json이 저장 정보를 불러오고 저장
     [SerializeField]
     public List<ProfileInfoText> infoTextList;
-        
-    [SerializeField]
-    public List<ProfileInfoPart> infoPartList; //profile, branch, starbook
 
     private ProfileInfoDataSO saveData;
 
-    [SerializeField]
-    private Button backButton;
 
     public void Init(ProfileInfoDataSO profileInfoDataSO)
     {
         saveData = profileInfoDataSO;
 
         Setting();
-        backButton.onClick.AddListener(OnBackButton);
     }
 
     public void Setting()//켰을때 기초 세팅
     {
-        foreach(var part in infoPartList)
-        {
-            if(part.isShow)
-            {
-                part.part.SetActive(true);
-            }
-            else
-            {
-                string str ="";
-                for(int i = 0; i < part.partNameKey.Length;i++)
-                {
-                    str += "?";
-                }
-                part.titleText.text = str;
-            }
-        }
+
+        //string str ="";
+        //for(int i = 0; i < part.partNameKey.Length;i++)
+        //{
+        //    str += "?";
+        //}
+        //part.titleText.text = str;
 
         foreach (var infoText in infoTextList)
         {
@@ -64,15 +49,15 @@ public class ProfileInfoPanel : MonoBehaviour
         }
         foreach (var save in saveData.saveList)
         {
-            if(save.isShow == false)
+            if (save.isShow == false)
             {
                 continue;
             }
 
-            foreach(var infoText in infoTextList)
+            foreach (var infoText in infoTextList)
             {
-                
-                if(infoText.infoNameKey == save.key)
+
+                if (infoText.infoNameKey == save.key)
                 {
                     infoText.ChangeText();
                 }
@@ -82,38 +67,34 @@ public class ProfileInfoPanel : MonoBehaviour
 
     public void Setting(string key)//특정어느것을 눌렀을때 세팅
     {
-        foreach (var part in infoPartList)
-        {
-            Debug.Log(part.partNameKey + " " + key);
-            if (key.Contains(part.partNameKey))
-            {
-                
-                //저장 방식이 어케되는거?
-                part.isShow = true;
-                part.part.SetActive(true);
-                part.titleText.text = part.partNameKey;
-            }
-        }
+        //foreach (var part in infoPartList)
+        //{
+        //    Debug.Log(part.partNameKey + " " + key);
+        //    if (key.Contains(part.partNameKey))
+        //    {
+
+        //        //저장 방식이 어케되는거?
+        //        part.isShow = true;
+        //        part.part.SetActive(true);
+        //        part.titleText.text = part.partNameKey;
+        //    }
+        //} 
+        //카테고리로 번경될 예정
 
         foreach (var infoText in infoTextList)
         {
-            if(infoText.infoNameKey == key)
+            if (infoText.infoNameKey == key)
             {
                 infoText.ChangeText();
                 Debug.Log(saveData);
                 Debug.Log(saveData.GetSaveData(key));
                 Debug.Log(key);
                 saveData.GetSaveData(key).isShow = true;
-                if(key == "OwnerName" && GameManager.Inst.GameState == EGameState.Tutorial)
+                if (key == "OwnerName" && GameManager.Inst.GameState == EGameState.Tutorial)
                 {
                     EventManager.TriggerEvent(ETutorialEvent.EndClickInfoTutorial);
                 }
             }
         }
-    }
-
-    private void OnBackButton()
-    {
-        this.gameObject.SetActive(false);
     }
 }
