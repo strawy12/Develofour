dcf7acf3f06eb632b6bcfb3e7fa1d9d3d3de4e76
OIF <hwaga0714@gmail.com>
2023-03-09 10:55:29 +0900

    SO 정리

--- Assets/02.Scripts/UI/Notice/ENoticeType.cs
+++ Assets/02.Scripts/UI/Notice/ENoticeType.cs
@@ -1,17 +1,7 @@
 public enum ENoticeType 
 {
 	None = -1,	
-    EndNewsCutScene,
-	PressHateButton,
-	EnterLoginSite,
-    SuccessLoginMailSite,
-	OpenPoliceMail,
-	OpenBrunchDeleteMail,
-	ClearPoliceMinigame,
-	OpenPoliceReplyMail,
-	EndBrunchPostDelete,
 	SnsSetNewPassword,
-	WriterFacebookLoginSuccess,
 	ConnectUSB,
     FindLaptopOwner,
     AiMessageAlarm,
