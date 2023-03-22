25800cd3f1141f82f84e568c4996a050eb371811
snowgaru <77821586+snowgaru@users.noreply.github.com>
2023-03-22 19:09:52 +0900

    Merge branch 'FileSoAddAndFix' of https://github.com/strawy12/Develofour into FileSoAddAndFix
    # Conflicts:
    완성
    #	Assets/03.Prefabs/ImageViewers/ImageViewer/Body/Pet/FirstMetPetsBody.prefab
    #	Assets/03.Prefabs/ImageViewers/ImageViewer/Body/Pet/WelcomePetsBody.prefab

--- Assets/02.Scripts/Window/Windows/Profile/ProfilePanel.cs
+++ Assets/02.Scripts/Window/Windows/Profile/ProfilePanel.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public enum EProfileCategory
 {
     None,
-    SuspectProfileInfomation,
-    SuspectProfileExtensionInfomation,
-    VictimProfileInfomation,
+    SuspectProfileInformation,
+    SuspectProfileExtensionInformation,
+    VictimProfileInformation,
     PetInformation,
     Count,
 }
@@ -28,7 +28,7 @@ public class ProfilePanel : MonoBehaviour
 
         EventManager.StartListening(EProfileEvent.FindInfoText, ChangeValue);
 
-
+        Debug.Log(infoList.Count);
         foreach(var info in infoList)
         {
             infoPanelList.Find(x => x.category == info.Key).Init(info.Value);
