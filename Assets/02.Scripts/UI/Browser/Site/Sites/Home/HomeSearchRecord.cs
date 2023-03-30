aaf627e2ab203b0272de004ab1eb8d706bc6e2fb
435yj <yujin050420@naver.com>
2023-03-30 20:17:50 +0900

    홈 레코드 정적 생성으로 바꿈

--- Assets/02.Scripts/UI/Browser/Site/Sites/Home/HomeSearchRecord.cs
+++ Assets/02.Scripts/UI/Browser/Site/Sites/Home/HomeSearchRecord.cs
@@ -6,47 +6,8 @@ using System;
 
 public class HomeSearchRecord : MonoBehaviour
 {
-    [SerializeField]
-    private HomeSearchRecordPrefab recordPrefab;
-    [SerializeField]
-    private int poolCnt;
-    [SerializeField]
-    private Transform contectTrm;
-    
-    private HomeSearchRecordDataSO recordData;
-
-    private List<HomeSearchRecordPrefab> poolPanels = new List<HomeSearchRecordPrefab>();
     public Action OnCloseRecord;
     private bool isOpen;
-
-    public void Init(HomeSearchRecordDataSO data)
-    {
-        if (poolPanels.Count < 5)
-        {
-            CreatePool();
-        }
-
-        recordData = data;
-        SettingString();
-    }
-    private void CreatePool()
-    {
-        for(int  i = 0; i < poolCnt; i++)
-        {
-            HomeSearchRecordPrefab obj = Instantiate(recordPrefab, contectTrm);
-            obj.Release();
-            poolPanels.Add(obj);
-            obj.gameObject.SetActive(false);
-        }
-    }
-
-    private void SettingString()
-    {
-        for(int i = 0; i < recordData.records.Count; i++)
-        {
-            poolPanels[i].Init(recordData.records[i]);
-        }
-    }
 
     public void OpenPanel()
     {
@@ -54,6 +15,7 @@ public class HomeSearchRecord : MonoBehaviour
         gameObject.SetActive(true);
         EventManager.StartListening(ECoreEvent.LeftButtonClick, CheckClose);
     }
+
     private void CheckClose(object[] hits)
     {
         if (!isOpen) return;
