cb0731729616843ae875ce7bed468ae797325256
435yj <yujin050420@naver.com>
2023-04-24 17:58:03 +0900

    Notice 판낼 글자 간격 조절

--- Assets/02.Scripts/UI/Utils/Debugger.cs
+++ Assets/02.Scripts/UI/Utils/Debugger.cs
@@ -32,6 +32,9 @@ public class Debugger : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
+            if (GameManager.Inst.GameState == EGameState.PlayTitle)
+                return;
+
             if(cutScene != null)
             {
                 if(!cutScene.isSkip)
