e85def5236a2161227e780ce94bce7d93fc41afc
snowgaru <77821586+snowgaru@users.noreply.github.com>
2023-04-26 00:41:22 +0900

    파일 열림

--- Assets/02.Scripts/Utils/Debugger.cs
+++ Assets/02.Scripts/Utils/Debugger.cs
@@ -76,6 +76,69 @@ public class Debugger : MonoBehaviour
         }
     }
 
+    public Sprite notepadSprite;
+    public Sprite BrowserSprite;
+    public Sprite harmonySprite;
+    public Sprite DirectorySprite;
+    public Sprite InstallerSprite;
+    public Sprite profileSprite;
+    public Sprite mediaPlayerSprite;
+    public Sprite siteShortcutSprite;
+    public Sprite harmonyShortcutSprite;
+
+    [ContextMenu("SetSOSprite")]
+    public void SS()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:FileSO", null);
+        foreach (string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            FileSO so = AssetDatabase.LoadAssetAtPath<FileSO>(path);
+            if(so.windowType == EWindowType.Notepad)
+            {
+                so.iconSprite = notepadSprite;
+            }
+            else if(so.windowType == EWindowType.Browser)
+            {
+                so.color = UnityEngine.Color.white;
+                so.iconSprite = BrowserSprite;
+            }
+            else if (so.windowType == EWindowType.Discord)
+            {
+                so.iconSprite = harmonySprite;
+            }
+            else if (so.windowType == EWindowType.Directory)
+            {
+                so.iconSprite = DirectorySprite;
+            }
+            else if (so.windowType == EWindowType.Installer)
+            {
+                so.iconSprite = InstallerSprite;
+            }
+            else if (so.windowType == EWindowType.ProfileWindow)
+            {
+                so.iconSprite = profileSprite;
+            }
+            else if (so.windowType == EWindowType.MediaPlayer)
+            {
+                so.iconSprite = mediaPlayerSprite;
+            }
+            else if (so.windowType == EWindowType.SiteShortCut)
+            {
+                so.color = UnityEngine.Color.white;
+                so.iconSprite = siteShortcutSprite;
+            }
+            else if (so.windowType == EWindowType.HarmonyShortCut)
+            {
+                so.iconSprite = harmonyShortcutSprite;
+            }
+            else if(so.windowType == EWindowType.ImageViewer)
+            {
+                so.color = UnityEngine.Color.white;
+            }
+        }
+    }
+
 #endif
 
 }
