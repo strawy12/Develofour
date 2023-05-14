18388825a28634365be9022d75c5d1d4556d683c
injun2005 <77712212+injun2005@users.noreply.github.com>
2023-05-14 23:27:43 +0900

    fileTagAdd code add

--- Assets/10.CustomWindows/Editor/FileSOSettingWindow/FileSOSettingWindow.cs
+++ Assets/10.CustomWindows/Editor/FileSOSettingWindow/FileSOSettingWindow.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 public class FileSOSettingWindow : EditorWindow
 {
@@ -74,6 +75,8 @@ public class FileSOSettingWindow : EditorWindow
             string pinHint = columns[6];
 
             List<int> childIdList = new List<int>();
+            string tagString = columns[7];
+            List<string> tags = tagString.Split(',').ToList();
 
             string[] children = columns[8].Split(',');
             foreach (string child in children)
@@ -104,13 +107,14 @@ public class FileSOSettingWindow : EditorWindow
                 file.name = columns[9];
                 isCreate = true;
             }
-
+            
             file.id = id;
             file.fileName = fileName;
             file.windowType = type;
             file.isFileLock = isFileLock;
             file.windowPin = pin;
             file.windowPinHintGuide = pinHint;
+            file.tags = tags;
 
             if (file is DirectorySO)
             {
