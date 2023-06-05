031b5baf96939fe8f04eaaaf3e5ba4a21220bb95
snowgaru <77821586+snowgaru@users.noreply.github.com>
2023-06-05 17:07:52 +0900

    no message

--- Assets/02.Scripts/UI/Browser/Site/Sites/Email/Emails/BranchCertificationMail.cs
+++ Assets/02.Scripts/UI/Browser/Site/Sites/Email/Emails/BranchCertificationMail.cs
@@ -9,6 +9,11 @@ public class BranchCertificationMail : Mail
     public override void Init()
     {
         base.Init();
+        if(btn == null)
+        {
+            Debug.Log("À¸¾ÇÀ¸¾Æ±×¾ÇÀ¸¾Æ±à°¡À¸¤¿¤¡");
+            return;
+        }
         btn.Init();
     }
 }
