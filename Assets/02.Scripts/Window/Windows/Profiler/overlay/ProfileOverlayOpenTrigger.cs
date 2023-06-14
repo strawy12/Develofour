4fab0e774799d5db9c7a1de157ad0e05bdfd2101
snowgaru <77821586+snowgaru@users.noreply.github.com>
2023-06-15 08:26:44 +0900

    오버레이 시스템 static 가지고있는거 주석

--- Assets/02.Scripts/Window/Windows/Profiler/overlay/ProfileOverlayOpenTrigger.cs
+++ Assets/02.Scripts/Window/Windows/Profiler/overlay/ProfileOverlayOpenTrigger.cs
@@ -18,7 +18,7 @@ public class ProfileOverlayOpenTrigger : MonoBehaviour
             isSetting = true;
             triggerCount.ForEach((trigger) => { trigger.fildID = fileID; });
         }
-        ProfileOverlaySystem.OnOpen?.Invoke(fileID, triggerCount);
+        //ProfileOverlaySystem.OnOpen?.Invoke(fileID, triggerCount);
     }
 
     //private void CheckClose(object[] hits)
@@ -31,7 +31,7 @@ public class ProfileOverlayOpenTrigger : MonoBehaviour
 
     public void Close()
     {
-        ProfileOverlaySystem.OnClose?.Invoke();
+        //ProfileOverlaySystem.OnClose?.Invoke();
     }
 
     //다른거 누를때 꺼지게하는거
