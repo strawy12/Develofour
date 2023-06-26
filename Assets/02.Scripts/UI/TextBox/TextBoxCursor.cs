35d63679468c8a6a1a5ef3e48240b11ed87311fd
OIF <hwaga0714@gmail.com>
2023-06-26 23:17:31 +0900

    텍스트 박스 커서 만드는 중

---
+++ Assets/02.Scripts/UI/TextBox/TextBoxCursor.cs
@@ -0,0 +1,47 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxCursor : MonoBehaviour
+{
+    private Image currentImage;
+    [SerializeField]
+    private float duration;
+
+    private bool isTurnOn;
+
+    public void Init()
+    {
+        currentImage = GetComponent<Image>();
+    }
+
+    public void TurnOn()
+    {
+        if (isTurnOn) return;
+
+        gameObject.SetActive(true);
+        isTurnOn = true;
+        StartCoroutine(FadeEffect());
+    }
+
+    public void TurnOff()
+    {
+        if (!isTurnOn) return;
+
+        isTurnOn = false;
+        StopAllCoroutines();
+        gameObject.SetActive(false);
+    }
+    private IEnumerator FadeEffect()
+    {
+        while(isTurnOn)
+        {
+            currentImage.DOFade(1f, duration);
+            yield return new WaitForSeconds(duration);
+            currentImage.DOFade(0f, duration);
+            yield return new WaitForSeconds(duration);
+        }
+    }
+}
