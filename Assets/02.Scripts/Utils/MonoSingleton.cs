c34e4d62c4eeb6914884b6e23eb5dd093f048c5d
OIF <hwaga0714@gmail.com>
2023-08-09 10:53:50 +0900

    싱글톤 locker 삭제함

--- Assets/02.Scripts/Utils/MonoSingleton.cs
+++ Assets/02.Scripts/Utils/MonoSingleton.cs
@@ -5,39 +5,20 @@ using UnityEngine;
 public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T _inst = null;
-    private static bool shuttingDown = false;
-    private static object locker = new object();
     public static T Inst
     {
         get
         {
-            if (shuttingDown)
+            if (_inst == null)
             {
-                Debug.LogWarning("[Singleton] Instance " + typeof(T) + " already destroyed. Returning null.");  
-            }
-
-            lock (locker)
-            {
+                _inst = FindObjectOfType<T>();
                 if (_inst == null)
                 {
-                    _inst = FindObjectOfType<T>();
-                    if (_inst == null)
-                    {
-                        _inst = new GameObject(typeof(T).ToString()).AddComponent<T>();
+                    _inst = new GameObject(typeof(T).ToString()).AddComponent<T>();
 
-                    }
                 }
             }
             return _inst;
         }
-    } 
-    private void OnDestroy()
-    {
-        shuttingDown = true;
-    }
-
-    private void OnApplicationQuit()
-    {
-        shuttingDown = true;
     }
 }
