c5ae65e9e92924d371795719ec3109ad2d5b45f0
snowgaru <77821586+snowgaru@users.noreply.github.com>
2023-09-12 14:53:23 +0900

    no message

---
+++ Assets/02.Scripts/Window/Windows/Profiler/Chatting/ProfilerAIText.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ProfilerAIText : MonoBehaviour
+{
+    public TMP_Text text;
+    public GameObject line;
+}
