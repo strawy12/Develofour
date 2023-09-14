68f624e1612da48121e811ff41efab9c9b727154
injun2005 <77712212+injun2005@users.noreply.github.com>
2023-09-14 10:49:10 +0900

    튜토리얼 통화 독백 수정

--- Assets/02.Scripts/Call/CallWindow/CallScreen/Incoming/C_A_I_1.cs
+++ Assets/02.Scripts/Call/CallWindow/CallScreen/Incoming/C_A_I_1.cs
@@ -7,8 +7,8 @@ public class C_A_I_1 : CallScreen
     public override void StartCall()
     {
         base.StartCall();
-        MonologSystem.AddOnEndMonologEvent("T_C_A_3", () => StopCall(true));
-        MonologSystem.OnStartMonolog?.Invoke("T_C_A_3", false);
+        MonologSystem.AddOnEndMonologEvent("T_C_A_6", () => StopCall(true));
+        MonologSystem.OnStartMonolog?.Invoke("T_C_A_6", false);
     }
     public override void StopCall(bool isClose)
     {
